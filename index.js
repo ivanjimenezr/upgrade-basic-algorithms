112f3982850f10c7047f40671b4f0b4f24ae93ee
Ivan Jimenez <chiklete@gmail.com>
2021-10-18 13:23:50 +0200

    Iteration#5

--- index.js
+++ index.js
@@ -102,3 +102,35 @@ console.log('1.6');
 const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 rickAndMortyCharacters3.splice(1,1);
 console.log(rickAndMortyCharacters3);
+
+//Iteración #5: Condicionales
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+console.log('COMPLETAR 1')
+if (number2 / number1 == 2) {
+    console.log("number2 dividido entre number1 es igual a 2");
+  }
+
+console.log('COMPLETAR 2')
+if (number1 !== number2) {
+    console.log("number1 es estrictamente distinto a number2");
+  }
+console.log('COMPLETAR 3')
+if (number3 != number1) {
+    console.log("number3 es distinto number1");
+  }
+console.log('COMPLETAR 4')
+if (number3 * 5 == number1) {
+    console.log("number3 por 5 es igual a number1");
+  }
+console.log('COMPLETAR 5')
+if (number3 * 5 == number1 || number1 * 2 == number2) {
+    console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+  }
+console.log('COMPLETAR 6')
+if (number2 / 2 == number1 && number1 / 5 == number3) {
+console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
