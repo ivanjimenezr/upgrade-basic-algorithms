31452ea3c58f3062aea543ac622889d8b34cff4c
Ivan Jimenez <chiklete@gmail.com>
2021-10-18 13:24:43 +0200

    Iteration#6

--- index.js
+++ index.js
@@ -134,3 +134,32 @@ console.log('COMPLETAR 6')
 if (number2 / 2 == number1 && number1 / 5 == number3) {
 console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
 }
+
+//Iteración #6: Bucles
+console.log('Iteración #6: Bucles')
+//1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+console.log('1.1')
+for (let index = 0; index <= 9; index++) {
+    console.log(index)
+  }
+
+//1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo cuando el resto del numero dividido entre 2 sea 0.
+console.log('1.2')
+for (let index = 0; index <= 9; index++) {
+    if (index % 2 == 0){
+        console.log(index)
+    }
+    
+  }
+
+//1.3 Crea un bucle para conseguir dormir contando ovejas. Este bucle empieza en 0 y termina en 10. Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle y cambia el mensaje en la última vuelta a 'Dormido!'.
+console.log('1.3')
+for (let index = 0; index <= 10; index++) {
+    if (index < 10){
+        console.log('Intentando dormir')
+    }else {
+        console.log('Dormido!')
+    }
+
+    
+  }
