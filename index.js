17785587e88d2807de2eba995961a53859012854
Ivan Jimenez <chiklete@gmail.com>
2021-10-18 13:22:23 +0200

    Iteration#3

--- index.js
+++ index.js
@@ -40,3 +40,29 @@ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 let cars = [car1, car2];
 
 console.log(cars)
+
+//Iteración #3: Operadores
+
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let c = 10 * 5;
+console.log('El resultado de la multiplicación es ' + c);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+let d = 10 / 2;
+console.log('El resultado de la división es ' + d);
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+let e = 15 % 9;
+console.log('El resultado del resto es ' + e);
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, teniendo dos variables y = 10 y z = 5.
+let f = 10;
+let g = 5;
+let i = f + g;
+console.log('El operador es la suma y el resultado de x es ' + i);
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50, teniendo dos variables y = 10 y z = 5.
+let j = 10;
+let k = 5;
+let l = j * 5;
+console.log('El operador es la multiplicación y el resultado de x es ' + l);
