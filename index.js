84fe1576fd988868795fe52ccd5cd41e1a07817c
Ivan Jimenez <chiklete@gmail.com>
2021-10-18 13:23:08 +0200

    Iteration#4

--- index.js
+++ index.js
@@ -66,3 +66,39 @@ let j = 10;
 let k = 5;
 let l = j * 5;
 console.log('El operador es la multiplicación y el resultado de x es ' + l);
+
+//Iteración #4: Arrays
+
+//1.1 Consigue el valor "HULK" del array de cars y muestralo por consola.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+let vengador = avengers[0];
+console.log('El valor de HULK es ' + vengador);
+
+//1.2 Cambia el primer elemento de avengers a "IRONMAN"
+const vengadores = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+vengadores[0] = "IRONMAN";
+console.log('El valor del array ahora es ' + vengadores);
+
+//1.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
+const avengers1 = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log('El array avengers1 tiene '+avengers1.length+' elementos')
+
+//1.4 Añade 2 elementos al array: "Morty" y "Summer".  Muestra en consola el último personaje del array
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push('Morty','Summer');
+console.log(rickAndMortyCharacters)
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length - 1])
+
+//1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+console.log('1.5 ');
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+//Eliminamos el último elemento del array, "Lapiz Lopez"
+rickAndMortyCharacters2.pop();
+console.log('El primer elemento del array es '+rickAndMortyCharacters2.shift()+' y el último es '+rickAndMortyCharacters2.pop());
+
+
+//1.6 Elimina el segundo elemento del array y muestra el array por consola.
+console.log('1.6');
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters3.splice(1,1);
+console.log(rickAndMortyCharacters3);
